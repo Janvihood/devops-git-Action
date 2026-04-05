f8d912c069c6350cce5ed913df839047c445fd66
janvihood <janvihood1@gmail.com>
2026-04-05 17:01:28 +0000

    trigger2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.write("Hello from CI/CD Pipeline 🚀");
+  res.write("Hello from CI/CD Pipeline januuu 🚀");
   res.end();
 }).listen(3000);
 //update
